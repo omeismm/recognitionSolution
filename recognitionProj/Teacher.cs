94256b02c0a0138358341d8785daffbce0e7b274
omeismm <128798266+omeismm@users.noreply.github.com>
2024-07-07 11:38:17 +0300

    typo

--- recognitionProj/Teacher.cs
+++ recognitionProj/Teacher.cs
@@ -4,7 +4,6 @@
     {
         //leave this commented until we learn how getters and setters work in csharp
         //private string Name;
-        //private string Name;
         //private string EduLevel; //bsc, msc, phd, etc...
         //private bool FullTime; //yes for fulltime, no for no
 
