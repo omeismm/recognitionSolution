a05cd7d5ba7a8f31422e33044b718bcee75c1692
omeismm <128798266+omeismm@users.noreply.github.com>
2024-07-07 11:34:33 +0300

    I made a class of teachers and a class of university

---
+++ recognitionProj/Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace recognitionProj
+{
+    public class Teacher
+    {
+        //leave this commented until we learn how getters and setters work in csharp
+        //private string Name;
+        //private string Name;
+        //private string EduLevel; //bsc, msc, phd, etc...
+        //private bool FullTime; //yes for fulltime, no for no
+
+
+    }
+}
---
+++ recognitionProj/University.cs
@@ -0,0 +1,11 @@
+﻿namespace recognitionProj
+{
+    public class University
+    {
+        //leave this commented until we learn how getters and setters work in csharp
+        //private string Name;
+        //private string Type; // medical or enginner on humanitarian etc..
+        //private int studentCount;
+        //private Teacher[] teachers;
+    }
+}
