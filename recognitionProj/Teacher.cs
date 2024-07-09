d004164f0f613edfb9a5eeabf4ecc532cddc883a
omeismm <128798266+omeismm@users.noreply.github.com>
2024-07-09 20:21:52 +0300

    constructor, getter, setter

--- recognitionProj/Teacher.cs
+++ recognitionProj/Teacher.cs
@@ -2,11 +2,45 @@
 {
     public class Teacher
     {
-        //leave this commented until we learn how getters and setters work in csharp
-        //private string Name;
-        //private string EduLevel; //bsc, msc, phd, etc...
-        //private bool FullTime; //yes for fulltime, no for no
+        private string Name;
+        private string EduLevel; //bsc, msc, phd, etc...
+        private bool FullTime; //yes for fulltime, no for no
 
+        public Teacher(string name, string eduLevel, bool fullTime)
+        {
+            Name = name;
+            EduLevel = eduLevel;
+            FullTime = fullTime;
+        }
 
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetEduLevel()
+        {
+            return EduLevel;
+        }
+
+        public void SetEduLevel(string eduLevel)
+        {
+            EduLevel = eduLevel;
+        }
+
+        public bool GetFullTime()
+        {
+            return FullTime;
+        }
+
+        public void SetFullTime(bool fullTime)
+        {
+            FullTime = fullTime;
+        }
     }
 }
--- recognitionProj/University.cs
+++ recognitionProj/University.cs
@@ -2,10 +2,57 @@
 {
     public class University
     {
-        //leave this commented until we learn how getters and setters work in csharp
-        //private string Name;
-        //private string Type; // medical or enginner on humanitarian etc..
-        //private int studentCount;
-        //private Teacher[] teachers;
+        private string Name;
+        private string Type; // medical or engineer or humanitarian etc..
+        private int studentCount;
+        private Teacher[] teachers;
+
+        public University(string name, string type, int studentCount, Teacher[] teachers)
+        {
+            Name = name;
+            Type = type;
+            this.studentCount = studentCount;
+            this.teachers = teachers;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetType()
+        {
+            return Type;
+        }
+
+        public void SetType(string type)
+        {
+            Type = type;
+        }
+
+        public int GetStudentCount()
+        {
+            return studentCount;
+        }
+
+        public void SetStudentCount(int count)
+        {
+            studentCount = count;
+        }
+
+        public Teacher[] GetTeachers()
+        {
+            return teachers;
+        }
+
+        public void SetTeachers(Teacher[] teachers)
+        {
+            this.teachers = teachers;
+        }
     }
 }
