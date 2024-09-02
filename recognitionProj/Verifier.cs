c27ea38a194e9cc893d62ab4342a7160c2db685a
omeismm <128798266+omeismm@users.noreply.github.com>
2024-09-02 09:15:37 +0300

    verifier

---
+++ recognitionProj/Verifier.cs
@@ -0,0 +1,24 @@
+﻿namespace recognitionProj
+{
+    public class Verifier//this class takes the university object and verivies everything in it
+    {
+        private University _uni;
+        private string[] _message;
+        public Verifier(University uni)
+        {
+            _uni = uni;
+            _message[0] = "Please run the verifier"; 
+        }
+        public string[] Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public University Uni
+        {
+            get { return _uni; }
+            set { _uni = value; }
+        }
+    }
+}
