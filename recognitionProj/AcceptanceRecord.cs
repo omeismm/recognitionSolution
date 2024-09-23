8fbc2ddc536aebe8f15a16b7b9dadddba2573337
omeismm <128798266+omeismm@users.noreply.github.com>
2024-09-23 08:38:16 +0300

    everything today was 0 new code, only adjustments

--- recognitionProj/AcceptanceRecord.cs
+++ recognitionProj/AcceptanceRecord.cs
@@ -1,39 +1,38 @@
-﻿namespace recognitionProj
+﻿namespace recognitionProj;
+
+public class AcceptanceRecord //this is to record the acceptance or decline of the university with the reason
 {
-    public class AcceptanceRecord //this is to record the acceptance or decline of the university with the reason
+    private bool _isAccepted;
+    private DateOnly _date;//date of record
+    private List<string> _reason;//reason of acceptance or decline. if it is declined because of fees and the university applies again, their application will be delayed by one semester (article 16 of pdf 22222)
+
+    public AcceptanceRecord(bool isAccepted, DateOnly date, List<string> reason)
     {
-        private bool _isAccepted;
-        private DateOnly _date;//date of record
-        private List<string> _reason;//reason of acceptance or decline. if it is declined because of fees and the university applies again, their application will be delayed by one semester (article 16 of pdf 22222)
+        _isAccepted = isAccepted;
+        _date = date;
+        _reason = reason;
+    }
 
-        public AcceptanceRecord(bool isAccepted, DateOnly date, List<string> reason)
-        {
-            _isAccepted = isAccepted;
-            _date = date;
-            _reason = reason;
-        }
+    public bool IsAccepted
+    {
+        get => _isAccepted;
+        set => _isAccepted = value;
+    }
 
-        public bool IsAccepted
-        {
-            get => _isAccepted;
-            set => _isAccepted = value;
-        }
+    public DateOnly Date
+    {
+        get => _date;
+        set => _date = value;
+    }
 
-        public DateOnly Date
-        {
-            get => _date;
-            set => _date = value;
-        }
+    public List<string> Reason
+    {
+        get => _reason;
+        set => _reason = value;
+    }
 
-        public List<string> Reason
-        {
-            get => _reason;
-            set => _reason = value;
-        }
-
-        public override string ToString()
-        {
-            return _isAccepted + " on " + _date + " because " + string.Join(", ", _reason);
-        }
+    public override string ToString()
+    {
+        return _isAccepted + " on " + _date + " because " + string.Join(", ", _reason);
     }
 }
