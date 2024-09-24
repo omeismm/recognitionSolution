9329ff2d069cbd04c46f7836747806bd25319667
omeismm <128798266+omeismm@users.noreply.github.com>
2024-09-24 13:32:48 +0300

    i added some commets

--- recognitionProj/AcceptanceRecord.cs
+++ recognitionProj/AcceptanceRecord.cs
@@ -2,7 +2,9 @@
 
 public class AcceptanceRecord //this is to record the acceptance or decline of the university with the reason
 {
-    private bool _isAccepted;
+    private bool _isAccepted;/// <summary>
+    /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// </summary>
     private DateOnly _date;//date of record
     private List<string> _reason;//reason of acceptance or decline. if it is declined because of fees and the university applies again, their application will be delayed by one semester (article 16 of pdf 22222)
 
