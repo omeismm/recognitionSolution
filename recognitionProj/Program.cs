da844077a66b7c5eb1747d025ff004a29cb7d015
omeismm <128798266+omeismm@users.noreply.github.com>
2024-12-04 16:22:56 +0300

    Comment out database initialization and query execution
    The code that initializes the database connection string and the
    DatabaseHandler object has been commented out. Additionally, the
    placeholder query execution using dbHandler.Query("SELECT * FROM
    my_table"); has also been commented out. This temporarily disables
    database-related functionality, likely for testing or debugging
    purposes. The rest of the code remains unchanged.

--- recognitionProj/Program.cs
+++ recognitionProj/Program.cs
@@ -6,10 +6,10 @@ public class Program
     {
         // open the database connection
 
-        var connectionString = "Host=my_host;Username=my_user;Password=my_pw;Database=my_db";//placeholders
-        DatabaseHandler dbHandler = new(connectionString);
+        //var connectionString = "Host=my_host;Username=my_user;Password=my_pw;Database=my_db";//placeholders
+        //DatabaseHandler dbHandler = new(connectionString);
         SpecializationController controller = new SpecializationController();
-        dbHandler.Query("SELECT * FROM my_table");//placeholder query
+        //dbHandler.Query("SELECT * FROM my_table");//placeholder query
         for ( int i=0; i<10; i++)
         {
             //hello world
