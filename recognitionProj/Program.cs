0ba172ff59800d80b3a8891ae17b0095797d8375
omeismm <128798266+omeismm@users.noreply.github.com>
2024-12-04 17:09:53 +0300

    added the css files, deleted unneded htmls

--- recognitionProj/Program.cs
+++ recognitionProj/Program.cs
@@ -1,8 +1,10 @@
 namespace recognitionProj;
 using RecognitionProj.Controllers;
+using System;
+using System.Diagnostics;
 public class Program
 {
-    public static void Main(string[] args)//this is the main
+    public static void Main(string[] args)
     {
         // open the database connection
 
@@ -10,11 +12,22 @@ public class Program
         //DatabaseHandler dbHandler = new(connectionString);
         SpecializationController controller = new SpecializationController();
         //dbHandler.Query("SELECT * FROM my_table");//placeholder query
-        for ( int i=0; i<10; i++)
+        for (int i = 0; i < 10; i++)
         {
             //hello world
             Console.WriteLine("Hello World!");
         }
-        
+
+        //open Mspec2.html and keep the program running
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "Mspec2.html",
+            UseShellExecute = true
+        });
+
+        while (true)
+        {
+            // Keep the program running
+        }
     }
 }
