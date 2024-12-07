d59ab5254e8920c2bdf63bd560a14ba043786706
omeismm <128798266+omeismm@users.noreply.github.com>
2024-12-07 08:21:39 +0300

    i started working on the backend frontend connection ie the server

--- recognitionProj/Program.cs
+++ recognitionProj/Program.cs
@@ -1,40 +1,61 @@
-namespace recognitionProj;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 using RecognitionProj.Controllers;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-public class Program
+using recognitionProj;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register controllers
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Assign a known URL
+app.Urls.Add("http://localhost:5000");
+
+// Map controller endpoints
+app.MapControllers();
+
+// Create controller and verifier instances as in your original code
+SpecializationController specController = new SpecializationController();
+Verifier verifier = new Verifier();
+
+// Print "Hello World!" once
+Console.WriteLine("Hello World!");
+
+// Retrieve High Diploma specializations from the controller
+var result = specController.GetSpecializationByType("High Diploma");
+
+if (result is OkObjectResult okResult 
+    && okResult.Value is List<Specialization> specializations 
+    && specializations.Count > 0)
 {
-    public static void Main(string[] args)
-    {
-        // open the database connection
+    var highDiplomaSpec = specializations[0];
+    Console.WriteLine(highDiplomaSpec.Color);
 
-        //var connectionString = "Host=my_host;Username=my_user;Password=my_pw;Database=my_db";//placeholders
-        //DatabaseHandler dbHandler = new(connectionString);
-        SpecializationController specController = new SpecializationController();
-        //dbHandler.Query("SELECT * FROM my_table");//placeholder query
-        Verifier verifier = new Verifier();
-        for (int i = 0; i < 10; i++)
-        {
-            //hello world
-            Console.WriteLine("Hello World!");
-        }
-
-        //open Mspec2.html and keep the program running
-        Process.Start(new ProcessStartInfo
-        {
-            FileName = "Mspec2.html",
-            UseShellExecute = true
-        });
-
-        //in memory list to store specializations temporarily
-        List<Specialization> SpecializationList = new List<Specialization>();
-
-        while (true)
-        {
-            specController.GetSpecializationByType("High Diploma");
-            SpecializationList = specController.SpecializationList;
-            Console.WriteLine(SpecializationList[0].Color);//todo fix this so the highdiploma comes out of the controller and into the main
-            specController.SaveSpecialization(SpecializationList[0]);
-        }
-    }
+    specController.SaveSpecialization(highDiplomaSpec);
 }
+else
+{
+    Console.WriteLine("No High Diploma specializations found.");
+}
+
+// Serve static files (make sure Mspec3.html is in wwwroot)
+app.UseStaticFiles();
+
+// Run the web server asynchronously
+var runTask = app.RunAsync();
+
+// After the server starts, open the Mspec3.html from the server on localhost
+Process.Start(new ProcessStartInfo
+{
+    FileName = "http://localhost:5000/Mspec3.html",
+    UseShellExecute = true
+});
+
+// Await the server running task to prevent the application from exiting
+await runTask;
