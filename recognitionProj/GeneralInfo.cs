920038510b3a2cee40557686976cc0ca2c7251bd
DeMont ML <DeMont_ML@outlook.com>
2024-12-31 20:31:13 +0300

    edited  the general cs for the metrics

--- recognitionProj/GeneralInfo.cs
+++ recognitionProj/GeneralInfo.cs
@@ -2,47 +2,46 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using System.Data;
 
-namespace recognitionProj
+namespace recognitionProj;
+
+public class GeneralInfo
 {
-    public class GeneralInfo // todo controller and db handler
+    private int _insID;
+    private int _attachID;  // Identity column
+    private string _attachName;
+    private string _attachDesc;
+
+    // Constructor
+    public GeneralInfo(int insID, int attachID, string attachName, string attachDesc)
     {
-        private int _insID;
-        private int _attachID;  // Identity column
-        private string _attachName;
-        private string _attachDesc;
+        _insID = insID;
+        _attachID = attachID;
+        _attachName = attachName;
+        _attachDesc = attachDesc;
+    }
 
-        // Constructor
-        public GeneralInfo(int insID, int attachID, string attachName, string attachDesc)
-        {
-            _insID = insID;
-            _attachID = attachID;
-            _attachName = attachName;
-            _attachDesc = attachDesc;
-        }
+    // Properties
+    public int InsID
+    {
+        get => _insID;
+        set => _insID = value;
+    }
 
-        // Properties
-        public int InsID
-        {
-            get => _insID;
-            set => _insID = value;
-        }
+    public int AttachID
+    {
+        get => _attachID;
+        set => _attachID = value;
+    }
 
-        public int AttachID
-        {
-            get => _attachID;
-            set => _attachID = value;
-        }
+    public string AttachName
+    {
+        get => _attachName;
+        set => _attachName = value;
+    }
 
-        public string AttachName
-        {
-            get => _attachName;
-            set => _attachName = value;
-        }
-
-        public string AttachDesc
-        {
-            get => _attachDesc;
-            set => _attachDesc = value;
-        }
+    public string AttachDesc
+    {
+        get => _attachDesc;
+        set => _attachDesc = value;
     }
 }
